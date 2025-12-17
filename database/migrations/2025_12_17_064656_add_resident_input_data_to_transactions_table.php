994efc18b695fd426c18121483cc6b9bc38bfd39
Virgilio F Tuga Jr <vftugajr@nemsu.edu.ph>
2025-12-17 14:47:31 +0800

    Try

---
+++ database/migrations/2025_12_17_064656_add_resident_input_data_to_transactions_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->json('resident_input_data')->nullable()->after('submitted_documents');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropColumn('resident_input_data');
+        });
+    }
+};
